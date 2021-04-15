2a4e0366e3412aa7aa97bd8874a2a38f5f0073d3
Ciaran Evans <ciaran@developmentseed.org>
2021-04-15 08:25:57 +0100

    Prepend path with github workspace

--- process_recipe.py
+++ process_recipe.py
@@ -4,7 +4,7 @@ import yaml
 
 
 def install_packages(path_to_meta_yml: str):
-    with open(path_to_meta_yml, "r") as file_in:
+    with open(f"/github/workspace/{path_to_meta_yml}", "r") as file_in:
         test_yaml = yaml.load(file_in, Loader=yaml.FullLoader)
         print(test_yaml["test"]["packages"])
 
